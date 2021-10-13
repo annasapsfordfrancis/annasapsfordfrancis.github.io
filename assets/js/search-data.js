f50a471e12039fe123aaaa6ffd658ddfdd15bc53
Anna Sapsford-Francis <83829719+annasapsfordfrancis@users.noreply.github.com>
2021-10-13 12:08:20 +0100

    Initial commit

---
+++ assets/js/search-data.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "Fiction",
+        "excerpt":"Soul-Eater A dark fantasy novel. Elska is turning into a monster… Elska should have died in the forest. That’s how the story was meant to go. She was to be the first victim of a creature of ancient and terrible evil: a soul-eater. Her fate was to die and be...","categories": [],
+        "tags": [],
+        "url": "/projects/fiction.html"
+      },{
+        "title": "Illustration",
+        "excerpt":"Sometimes I draw sassy birds under the name AdventureBirds. You can find my RedBubble store here.  ","categories": [],
+        "tags": [],
+        "url": "/projects/illustration.html"
+      },{
+        "title": "Pixel Art",
+        "excerpt":"I make pixel art for games, for people and for fun. You can see my Instagram here. I’m currently open for commissions. You can contact me here.  ","categories": [],
+        "tags": [],
+        "url": "/projects/pixel-art.html"
+      }]
